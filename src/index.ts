f95743c1b39d9509593c72460f5b419e3a9ced42
saiHarshaX <saiharsha9897@gmail.com>
2025-04-19 16:35:24 +0530

    perf(bootstrap): bootstrap

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
+import { BootstrapConfig } from './config/bootstrap.config';
 import { AppConfigModule } from './config/config.module';
 import { AppConfigService } from './config/config.service';
 
-export { AppConfigModule, AppConfigService };
+export { AppConfigModule, AppConfigService, BootstrapConfig };
